af6fc37deb893c892763d92410c688601227d95e
Mariano Meissner <marianome@gmail.com>
2021-08-09 18:37:57 +0200

    feat: userClub endpoint

---
+++ src/controllers/UserClub.php
@@ -0,0 +1,56 @@
+<?php
+
+    namespace Elitesports;
+
+    use Elitesports\Respuestas;
+    use stdClass;
+
+
+    class UserClub{
+
+        private $user;
+        private $token;
+        private $respuestas;
+    
+        public function __construct()
+        {
+            $this->userClub = new \Elitelib\UserClub();
+            $this->token = new \Elitelib\Token();
+            $this->respuestas = new Respuestas(); 
+        }
+
+
+        public function getOwnClub($json){ 
+            
+            $datos = json_decode($json, true);
+
+            if( !isset($datos['token'])){
+                return $this->respuestas->error_401();
+            }else{
+                                
+                $testToken = $this->token->checkToken($datos['token']);               
+
+                if($testToken){                   
+
+                        $club_id = $this->userClub->getUserClub( $testToken[0]['user_id'] );
+
+                        $resultado = new stdClass(); 
+                        $resultado->status = "ok";                       
+                        $resultado->result = new stdClass();
+                        $resultado->result = $club_id;
+                        
+                        return $resultado;                                      
+                      
+                
+                }else{
+                    return $this->respuestas->error_401("Token invalid or expired");
+                }
+
+            }           
+
+        }
+       
+
+
+
+    }
