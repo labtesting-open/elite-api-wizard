ec21d1a428e111012f5165ab2c0160094e65afeb
Mariano Meissner <marianome@gmail.com>
2021-08-17 13:47:41 +0200

    fix: get token added in each test request

--- tests/ClubTest.php
+++ tests/ClubTest.php
@@ -23,15 +23,30 @@ class ClubTest extends TestCase
     {
 
         try {
+            $requestToken = $this->client->request(
+                'POST',
+                'http://localhost/labtest/elite-api-wizard/v1/login.php',
+                [
+                'body' => '{
+                    "user":"elitesports17",
+                    "password":"abc1234"
+                }']
+            );
+        
+            $response = json_decode($requestToken->getBody()->getContents());
+
+            $token = $response->result->token;
+
+            $body = '{"token":"' . $token . '","club_id":"1"}';
+            
             $requestBasic = $this->client->request(
                 'POST',
                 'http://localhost/labtest/elite-api-wizard/v1/club.php',
                 [
-                'body' => '{
-                    "token":"85c6dc7d5922cb381f8eb8a82671d6e9",
-                    "club_id":"1"
-                }']
+                'body' => $body
+                ]
             );
+
         
             $response = json_decode($requestBasic->getBody()->getContents());
         
