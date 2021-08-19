6b6a6e7579f9cad6dd2359ccdac96f42a5a01565
Mariano Meissner <marianome@gmail.com>
2021-08-19 17:46:19 +0200

    fix: xdebug coverage

--- src/controllers/Setting.php
+++ src/controllers/Setting.php
@@ -33,7 +33,7 @@ class Setting
 
     public function setRemoteMode()
     {
-        $this->server = 'http://e79101d38d16.ngrok.io';
+        $this->server = 'http://86b5194286db.ngrok.io';
         $this->user = 'elitesports17';
         $this->password = 'abc1234';
         $this->folder = '/elite-api-wizard';
