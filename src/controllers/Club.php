9eba5bee7bf0bb03c6876d74a8374dfbcbfc67b1
Mariano Meissner <marianome@gmail.com>
2021-08-24 18:44:29 +0200

    fix: class club duplicate code

--- src/controllers/Club.php
+++ src/controllers/Club.php
@@ -18,9 +18,8 @@ class Club
         $this->respuestas  = new Respuestas();
     }
 
-    public function getInfo($json)
+    private function checkTokenAndResponse($json)
     {
-           
         $datos = json_decode($json, true);
         $responseHttp = $this->respuestas->error401();
         
@@ -28,79 +27,89 @@ class Club
             $testToken = $this->token->checkToken($datos['token']);
 
             if ($testToken) {
-                if (isset($datos['club_id']) && is_numeric($datos['club_id'])) {
-                    $info = $this->club->getBasicInfo($datos['club_id']);
-
-                    $resultado = new stdClass();
-                    $resultado->status = 'ok';
-                    $resultado->result = new stdClass();
-                    $resultado->result = $info;
-                        
-                    $responseHttp = $resultado;
-                } else {
-                    $responseHttp = $this->respuestas->error200(ResponseHttp::DATAINCORRECTORINCOMPLETE);
-                }
+                $responseHttp = null;
             } else {
                 $responseHttp = $this->respuestas->error401(ResponseHttp::TOKENINVALIDOREXPIRED);
             }
         }
         return $responseHttp;
     }
-    
-    public function getInfoWithFilters($json)
+
+    public function getInfo($json)
     {
-           
-        $datos = json_decode($json, true);
-        $responseHttp = $this->respuestas->error401();
+        $responseHttp = $this->checkTokenAndResponse($json);
 
-        if (isset($datos['token'])) {
-            $testToken = $this->token->checkToken($datos['token']);
-
-            if ($testToken) {
-                $continentCode = null;
-                if (isset($datos['continent_code']) &&  !empty($datos['continent_code'])) {
-                    $continentCode = $datos['continent_code'];
-                }
-                $countryCode = null;
-                if (isset($datos['country_code']) &&  !empty($datos['country_code'])) {
-                    $countryCode = $datos['country_code'];
-                }
-                $categoryId = null;
-                if (isset($datos['category_id']) &&  !empty($datos['category_id'])) {
-                    $categoryId = $datos['category_id'];
-                }
-                $divisionId = null;
-                if (isset($datos['division_id']) &&  !empty($datos['division_id'])) {
-                    $divisionId = $datos['division_id'];
-                }
-                $page = 1;
-                if (isset($datos['page']) &&  is_numeric($datos['page'])) {
-                    $page = $datos['page'];
-                }
-                $cant = 100;
-                if (isset($datos['cant']) &&  is_numeric($datos['cant'])) {
-                    $cant = $datos['cant'];
-                }
-                
-                $info = $this->club->getClubsByFilters(
-                    $continentCode,
-                    $countryCode,
-                    $categoryId,
-                    $divisionId,
-                    $page,
-                    $cant
-                );
+        if ($responseHttp == null) {
+            $datos = json_decode($json, true);
+            if (isset($datos['club_id']) && is_numeric($datos['club_id'])) {
+                $info = $this->club->getBasicInfo($datos['club_id']);
 
                 $resultado = new stdClass();
                 $resultado->status = 'ok';
                 $resultado->result = new stdClass();
                 $resultado->result = $info;
-                        
-                $responseHttp =  $resultado;
+                    
+                $responseHttp = $resultado;
             } else {
-                $responseHttp = $this->respuestas->error401(ResponseHttp::TOKENINVALIDOREXPIRED);
+                $responseHttp = $this->respuestas->error200(ResponseHttp::DATAINCORRECTORINCOMPLETE);
             }
         }
+
         return $responseHttp;
     }
+    
+    public function getInfoWithFilters($json)
+    {
+
+        $responseHttp = $this->checkTokenAndResponse($json);
+
+        if ($responseHttp == null) {
+            $datos = json_decode($json, true);
+
+            $continentCode = null;
+            if (isset($datos['continent_code']) &&  !empty($datos['continent_code'])) {
+                $continentCode = $datos['continent_code'];
+            }
+            $countryCode = null;
+            if (isset($datos['country_code']) &&  !empty($datos['country_code'])) {
+                $countryCode = $datos['country_code'];
+            }
+            $categoryId = null;
+            if (isset($datos['category_id']) &&  !empty($datos['category_id'])) {
+                $categoryId = $datos['category_id'];
+            }
+            $divisionId = null;
+            if (isset($datos['division_id']) &&  !empty($datos['division_id'])) {
+                $divisionId = $datos['division_id'];
+            }
+            $page = 1;
+            if (isset($datos['page']) &&  is_numeric($datos['page'])) {
+                $page = $datos['page'];
+            }
+            $cant = 100;
+            if (isset($datos['cant']) &&  is_numeric($datos['cant'])) {
+                $cant = $datos['cant'];
+            }
+            
+            $info = $this->club->getClubsByFilters(
+                $continentCode,
+                $countryCode,
+                $categoryId,
+                $divisionId,
+                $page,
+                $cant
+            );
+
+            $resultado = new stdClass();
+            $resultado->status = 'ok';
+            $resultado->result = new stdClass();
+            $resultado->result = $info;
+                    
+            $responseHttp =  $resultado;
+        }
+
+        return $responseHttp;
+    
+    }
+    
 }
