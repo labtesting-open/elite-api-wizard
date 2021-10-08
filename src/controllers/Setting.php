ae9a6ae811b5ea2df2a29879c6a83763fe51691d
Mariano Meissner <marianome@gmail.com>
2021-10-08 16:42:07 +0200

    fix: new ngrok key

--- src/controllers/Setting.php
+++ src/controllers/Setting.php
@@ -33,7 +33,7 @@ class Setting
 
     public function setRemoteMode()
     {
-        $this->server = 'http://b1fa-83-39-205-111.ngrok.io';
+        $this->server = 'http://4f12-83-39-205-111.ngrok.io';
         $this->user = 'admin@wizard.com';
         $this->password = 'abc1234';
         $this->folder = '/elite-api-wizard';
