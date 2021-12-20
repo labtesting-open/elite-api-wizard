e8683bbb7b82455ef8cc9826498b176fce5998d4
Mariano Meissner <marianome@gmail.com>
2021-12-20 12:41:54 +0100

    fix: category and Division reduce duplication code

--- src/controllers/Category.php
+++ src/controllers/Category.php
@@ -27,43 +27,25 @@ class Category
     
     public function getAvailableCategories($json)
     {
+        $paramsReceived = json_decode($json, true);
 
-        $responseHttp = $this->checkTokenAndReturnResponse($json);
+        $paramsAcepted = array(
+            'continent_code' => null,
+            'country_code' => null,
+            'category_id' => null,
+            'division_id' => null
+        );
 
-        if (is_null($responseHttp)) {
-            $datos = json_decode($json, true);
+        $paramsNormaliced = Utils::normalizerParams($paramsReceived, $paramsAcepted);        
 
-            $continentCode = null;
-            if (isset($datos['continent_code']) &&  !empty($datos['continent_code'])) {
-                $continentCode = $datos['continent_code'];
-            }
-            $countryCode = null;
-            if (isset($datos['country_code']) &&  !empty($datos['country_code'])) {
-                $countryCode = $datos['country_code'];
-            }
-            $categoryId = null;
-            if (isset($datos['category_id']) &&  !empty($datos['category_id'])) {
-                $categoryId = $datos['category_id'];
-            }
-            $divisionId = null;
-            if (isset($datos['division_id']) &&  !empty($datos['division_id'])) {
-                $divisionId = $datos['division_id'];
-            }
-            
-            $info = $this->category->getAvailableCategories(
-                $continentCode,
-                $countryCode,
-                $categoryId,
-                $divisionId
-            );
+        $result = $this->category->getAvailableCategories(
+            $paramsNormaliced['continent_code'],
+            $paramsNormaliced['country_code'],
+            $paramsNormaliced['category_id'],
+            $paramsNormaliced['division_id']
+        );
 
-            $resultado = new stdClass();
-            $resultado->status = 'ok';
-            $resultado->result = new stdClass();
-            $resultado->result = $info;
-                    
-            $responseHttp =  $resultado;
-        }
+        $responseHttp = $this->respuestas->standarSuccess($result);       
 
         return $responseHttp;
     }
